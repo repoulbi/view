90af7acc0227976aa347d1c7263e1167302ba5fa
HeroBuxx <me@herobuxx.me>
2024-02-07 16:46:53 +0700

    feat: Ad method to handle URL Hash
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 import {displayConcatenatedPDFs} from "./lib.js"
 
-displayConcatenatedPDFs();
+const urlHash = window.location.hash;
+const hashParams = urlHash.substring(1).split('&');
+let pdfA, pdfB;
+
+hashParams.forEach(param => {
+    // Assuming param directly contains PDF URLs without keys
+    const value = decodeURIComponent(param);
+    if (!pdfA) {
+        pdfA = value;
+    } else if (!pdfB) {
+        pdfB = value;
+    }
+});
+
+displayConcatenatedPDFs(pdfA, pdfB)
