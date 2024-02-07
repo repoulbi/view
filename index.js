ea7a404d0d08e25a494e5277444bf817cb702eee
HeroBuxx <me@herobuxx.me>
2024-02-07 16:23:05 +0700

    feat: Call PDF cocat
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-import {createPdf,mergeAllPDFs,embedPdfPages} from "https://repo.ulbi.ac.id/lkd/lib.js"
+import {displayConcatenatedPDFs} from "./lib.js"
 
-//createPdf();
-
-const docs = ["https://repo.ulbi.ac.id/sk/2324-1/402_PengampuMK.pdf","https://repo.ulbi.ac.id/lkd/POBKD.pdf"];
-//mergeAllPDFs(docs);
-embedPdfPages();
+displayConcatenatedPDFs();
