36d5684c555e823dfb76202bdf8129ce9f34e705
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-07 13:36:29 +0700

    mergeAllPDFs

--- index.js
+++ index.js
@@ -27,4 +27,37 @@ async function merge() {
     const mergedPdfFile = await mergedPdf.save();
 
     download(mergedPdfFile, 'pdf-lib_page_copying_example.pdf', 'application/pdf')
+}
+
+async function mergeAllPDFs(urls) {
+        
+    // create an empty PDFLib object of PDFDocument to do the merging into
+    const pdfDoc = await PDFLib.PDFDocument.create();
+    
+    // iterate over all documents to merge
+    const numDocs = urls.length;    
+    for(var i = 0; i < numDocs; i++) {
+
+        // download the document
+        const donorPdfBytes = await fetch(urls[i]).then(res => res.arrayBuffer());
+
+        // load/convert the document into a PDFDocument object
+        const donorPdfDoc = await PDFLib.PDFDocument.load(donorPdfBytes);
+
+        // iterate over the document's pages
+        const docLength = donorPdfDoc.getPageCount();
+        for(var k = 0; k < docLength; k++) {
+            // extract the page to copy
+            const [donorPage] = await pdfDoc.copyPages(donorPdfDoc, [k]);
+
+            // add the page to the overall merged document
+            pdfDoc.addPage(donorPage);
+        }
     }
+    
+    // save as a Base64 URI
+    const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+
+    // strip off the first part to the first comma "data:image/png;base64,iVBORw0K..."
+    const data_pdf = pdfDataUri.substring(pdfDataUri.indexOf(',')+1);
+}
