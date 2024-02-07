bebe5070e389ce8faef231029579372d80fce781
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-07 13:55:37 +0700

    coba merge

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-import {createPdf} from "https://repo.ulbi.ac.id/lkd/lib.js"
-createPdf();
+import {createPdf,mergeAllPDFs} from "https://repo.ulbi.ac.id/lkd/lib.js"
+
+//createPdf();
+
+mergeAllPDFs("https://repo.ulbi.ac.id/sk/2324-1/402_PengampuMK.pdf","https://repo.ulbi.ac.id/lkd/POBKD.pdf");
