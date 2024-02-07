5cc979b8549d1f2f2f57b10a87f1d0e77b5a4190
HeroBuxx <me@herobuxx.me>
2024-02-07 16:47:40 +0700

    feat: Align params with newer changes for URL Hases
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- lib.js
+++ lib.js
@@ -1,11 +1,12 @@
 import { PDFDocument, PDFPage } from 'https://cdn.skypack.dev/pdf-lib';
 
-const flagUrl = 'https://repo.ulbi.ac.id/sk/2324-1/402_PengampuMK.pdf';
-const constitutionUrl = 'https://repo.ulbi.ac.id/lkd/POBKD.pdf';
-
 const pdfIframe = document.getElementById('pdf');
 
-export async function displayConcatenatedPDFs() {
+export async function displayConcatenatedPDFs(pdfA, pdfB) {
+
+  const flagUrl = 'https://repo.ulbi.ac.id/sk/2324-1/' + pdfA + '.pdf';
+  const constitutionUrl = 'https://repo.ulbi.ac.id/lkd/' + pdfB + '.pdf';
+
   try {
     const flagPdfBytes = await fetch(flagUrl).then(response => response.arrayBuffer());
     const constitutionPdfBytes = await fetch(constitutionUrl).then(response => response.arrayBuffer());
