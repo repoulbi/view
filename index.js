9bf029f552521dbe4e63854208e114287c7e7a25
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-07 14:00:19 +0700

    array

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ import {createPdf,mergeAllPDFs} from "https://repo.ulbi.ac.id/lkd/lib.js"
 
 //createPdf();
 
-mergeAllPDFs("https://repo.ulbi.ac.id/sk/2324-1/402_PengampuMK.pdf","https://repo.ulbi.ac.id/lkd/POBKD.pdf");
+const docs = ["https://repo.ulbi.ac.id/sk/2324-1/402_PengampuMK.pdf","https://repo.ulbi.ac.id/lkd/POBKD.pdf"];
+
+mergeAllPDFs(docs);
