7d2b429319b886a4150ea5b9fb71be5a8cb28cc6
HeroBuxx <me@herobuxx.me>
2024-02-12 11:46:37 +0700

    feat: Handle undefined hash URL Value
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { displayConcatenatedPDFs } from "./lib.js";
+import { displayConcatenatedPDFs, displaySinglePDF } from "./lib.js";
 
 const urlHash = window.location.hash;
 if (!urlHash) {
@@ -12,12 +12,37 @@ let pdfA, pdfB;
 hashParams.forEach(param => {
     // Assuming param directly contains PDF URLs without keys
     const value = decodeURIComponent(param);
-    if (!pdfA) {
+    if (value.startsWith('402_')) {
         pdfA = value;
-    } else if (!pdfB) {
+    } else if (value.startsWith('BAP')) {
         pdfB = value;
     }
 });
 
-// Load and display concatenated PDFs
-displayConcatenatedPDFs(pdfA, pdfB);
+
+if (pdfA === undefined) {
+    console.log('pdfA Value: ', pdfA);
+    console.log('pdfB Value: ', pdfB);
+    displaySinglePDF("B", pdfB);
+} else if (pdfB === undefined) {
+    console.log('pdfA Value: ', pdfA);
+    console.log('pdfB Value: ', pdfB);
+    displaySinglePDF("A", pdfA);
+} else {
+    console.log('pdfA Value: ', pdfA);
+    console.log('pdfB Value: ', pdfB);
+    displayConcatenatedPDFs(pdfA, pdfB);
+}
+
+/*
+// Check if at least one PDF URL is provided
+if (pdfA || pdfB) {
+    // Load and display concatenated PDFs
+    console.log('pdfA Value: ', pdfA);
+    console.log('pdfB Value: ', pdfB);
+    displayConcatenatedPDFs(pdfA, pdfB);
+} else {
+    // Handle case where no PDF URLs are provided
+    console.error('No valid PDF URLs provided');
+}
+*/
