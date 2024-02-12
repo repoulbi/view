4237e698f0c3c9b5080b7dcdcffe9f8e35077dee
HeroBuxx <me@herobuxx.me>
2024-02-12 15:49:44 +0700

    feat: Encode Hash URL value using base64
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import { displayConcatenatedPDFs } from "./lib.js";
 
-const urlHash = window.location.hash;
-if (!urlHash) {
+const decodedURL = atob(decodeURIComponent(window.location.hash.substring(1)));
+
+console.log("A2B: ", decodedURL);
+
+if (!decodedURL) {
     // If hash URL is empty, redirect to 404.html
     window.location.href = '404.html';
 }
 
-const hashParams = urlHash.substring(1).split('&');
+const hashParams = decodedURL.substring(1).split('&');
 const pdfUrls = [];
 
 hashParams.forEach(param => {
